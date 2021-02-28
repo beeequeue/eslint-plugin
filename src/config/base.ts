1a8ebd5a9547dd9136a4295518a5b8fa510c7362
Adam <adam.lj.haglund@gmail.com>
2021-02-28 22:34:50 +0100

    add eslint, import rules

--- src/config/base.ts
+++ src/config/base.ts
@@ -21,6 +21,79 @@ export const base: Linter.BaseConfig<ESLintRules> = {
     "import/internal-regex": "^@*/**/*",
   },
   rules: {
+    /* ESLINT */
+
+    /** Enforce camelCase */
+    camelcase: "error",
+    /** Always use === */
+    eqeqeq: "error",
+    /** Require using const and let */
+    "no-var": "error",
+    /** Require using object shorthands */
+    "object-shorthand": "error",
+    /** Require const on variables that are not reassigned */
+    "prefer-const": "error",
+    /** Require destructuring where possible */
+    "prefer-destructuring": "error",
+    /** Require using template strings */
+    "prefer-template": "error",
+    /** Create utilities rather than extending native objects */
+    "no-extend-native": "error",
+    /** https://eslint.org/docs/rules/no-else-return */
+    "no-else-return": "error",
+    /** Don't allow optional chaining where it might create runtime errors */
+    "no-unsafe-optional-chaining": "error",
+    /** Disallow code that we know might result in floating point errors */
+    "no-loss-of-precision": "error",
+    /** Require if/for/when/while blocks to have curly brackets, unless they're one-liners */
+    // @ts-ignore
+    curly: ["error", "multi", "consistent"],
+
+    /* IMPORT PLUGIN */
+
+    /** Extremely slow in some cases */
+    "import/no-deprecated": "off",
+
+    /** Disallow duplicate imports */
     "import/no-duplicated": "error",
+
+    /** Custom import order with quick fix */
+    "import/order": [
+      "error",
+      {
+        alphabetize: { order: "asc" },
+        groups: [
+          "builtin",
+          ["external", "internal", "unknown"],
+          "parent",
+          ["sibling", "index"],
+          "object",
+        ],
+        pathGroups: [
+          {
+            pattern: "@/**/*",
+            group: "internal",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        "newlines-between": "always",
+      },
+    ],
+
+    /** Only allow devDependency imports in test files */
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          "**/*test*/**/*",
+          "**/*.test.*",
+          "src/testing/**/*",
+          "**/*stories*",
+          "*.{config,setup}.*",
+          "**/test-utils.ts",
+        ],
+      },
+    ],
   },
 }
