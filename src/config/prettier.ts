a0bc302877f1a9fe436f765f528f295414305ddc
Adam <adam.lj.haglund@gmail.com>
2021-03-01 15:43:21 +0100

    fix typo, order of prettier plugin

--- src/config/prettier.ts
+++ src/config/prettier.ts
@@ -5,14 +5,14 @@ import { ESLintRules } from "eslint/rules"
 export const prettier: Linter.BaseConfig<ESLintRules> = {
   extends: [
     /**
-     * Enables plugin, adds rule for styles
-     * https://github.com/prettier/eslint-plugin-prettier
-     */
-    'plugin:prettier/recommended"',
-    /**
      * Disables style rules from other plugins/configs
      * https://github.com/prettier/eslint-config-prettier
      */
     "prettier",
+    /**
+     * Enables plugin, adds rule for styles
+     * https://github.com/prettier/eslint-plugin-prettier
+     */
+    "plugin:prettier/recommended",
   ],
 }
