a9d7880e346b514ec6b73572af4156aa4d408426
Adam <adam@haglund.dev>
2021-08-29 19:21:43 +0200

    update more stuff

--- src/config/vue.ts
+++ src/config/vue.ts
@@ -12,5 +12,17 @@ export const vue: Linter.BaseConfig<ESLintRules> = {
   rules: {
     /** Allow using v-html to insert html strings. We should know when this is necessary and when it's not. */
     "vue/no-v-html": "off",
+
+    /* Setup support */
+
+    "vue/script-setup-uses-vars": "error",
+    "vue/no-export-in-script-setup": "error",
+    "vue/require-expose": "error",
+    "vue/valid-define-props": "error",
+    "vue/valid-define-emits": "error",
+
+    /* Deprecated */
+
+    "vue/no-deprecated-v-is": "error",
   },
 }
