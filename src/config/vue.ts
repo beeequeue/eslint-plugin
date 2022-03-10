3ebf87f65fb6ee00b6a85bf84608cf7126f8c305
Adam <adam@haglund.dev>
2022-03-10 20:18:22 +0100

    update vue plugin

--- src/config/vue.ts
+++ src/config/vue.ts
@@ -12,21 +12,27 @@ export const vue: Linter.BaseConfig<ESLintRules> = {
   rules: {
     /** Allow using v-html to insert html strings. We should know when this is necessary and when it's not. */
     "vue/no-v-html": "off",
-    /** Self-explanatory */
-    "vue/no-useless-template-attributes": "error",
-    "vue/no-computed-properties-in-data": "error",
+    /** Warn when passing children into a component that overwrites them. */
+    "vue/no-child-content": "error",
+    /** Enforce consistent component naming in component options. */
+    "vue/component-options-name-casing": "error",
+    /** Enforce static classes be in separate class attribute from dynamic ones. */
+    "vue/prefer-separate-static-class": "error",
+    /** Forbid using attributes that break components on them. */
+    "vue/no-v-text-v-html-on-component": "error",
+    /** Warn when using a component without configuring it in the options. */
+    "vue/no-undef-components": "warn",
+    /** Enforce using the `true` attribute shorthand. */
+    "vue/prefer-true-attribute-shorthand": "error",
+
+    /* Style rules */
+
+    "vue/object-shorthand": "off",
 
     /* Setup support */
 
     "vue/script-setup-uses-vars": "error",
-    "vue/no-export-in-script-setup": "error",
     "vue/require-expose": "error",
-    "vue/valid-define-props": "error",
-    "vue/valid-define-emits": "error",
-
-    /* Deprecated */
-
-    "vue/no-deprecated-v-is": "error",
-    "vue/no-deprecated-router-link-tag-prop": "error",
+    "vue/no-expose-after-await": "error",
   },
 }
